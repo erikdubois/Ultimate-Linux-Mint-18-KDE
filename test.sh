b0ea9ffe0be8b818cfd61c75da98ac373bf78801
Erik Dubois <erik.dubois@gmail.com>
2016-08-18 22:32:21 +0200

    Commit comment :  on Thu Aug 18 22:32:21 CEST 2016

---
+++ test.sh
@@ -0,0 +1,124 @@
+#!/bin/bash
+#
+#                                       
+# MMMMMMMMMMMMMMMMMMMMMMMMMmds+.        
+# MMm----::-://////////////oymNMd+`     
+# MMd      /++                -sNMd:    
+# MMNso/`  dMM    `.::-. .-::.` .hMN:   
+# ddddMMh  dMM   :hNMNMNhNMNMNh: `NMm   
+#     NMm  dMM  .NMN/-+MMM+-/NMN` dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  -MMm  `MMM   dMM. dMM   
+#     NMm  dMM  .mmd  `mmm   yMM. dMM   
+#     NMm  dMM`  ..`   ...   ydm. dMM   
+#     hMM- +MMd/-------...-:sdds  dMM   
+#     -NMm- :hNMNNNmdddddddddy/`  dMM   
+#      -dMNs-``-::::-------.``    dMM   
+#       `/dMNmy+/:-------------:/yMMM  
+#          ./ydNMMMMMMMMMMMMMMMMMMMMM  
+#             \.MMMMMMMMMMMMMMMMMMM    
+#                                      
+#
+#
+##################################################################################################################
+#
+# Current project : Ultimate-Linux-Mint-18
+#
+# Source 	: 	https://github.com/erikdubois/Ultimate-Linux-Mint-18
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+# 
+# More from Erik Dubois
+#
+# Aurora Conky
+# at http://sourceforge.net/projects/auroraconkytheme/
+# Explanation on the use of this theme can be found at 
+# http://erikdubois.be/category/linux/aurora-conky/
+# 
+# Aureola Conky
+# Collections of nice conky's with lua syntax
+# https://github.com/erikdubois/Aureola
+#
+# Sardi icons
+# Many different styles of icons from colourfull, monochrome, white, circle
+# https://sourceforge.net/projects/sardi/
+#
+# Super Ultra Flat Numix Remix
+# Colourfull and playfull icons
+# https://github.com/erikdubois/Super-Ultra-Flat-Numix-Remix
+#
+# Check out the github - many more scripts for automatic installation of Linux Systems.
+#
+#
+#
+#
+#
+##################################################################################################################
+# If the option -y has been added. It will autoinstall all. Omit if you do not want that.
+##################################################################################################################
+#
+#
+#
+#
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+
+
+
+
+###############################################################################################
+
+#software from 'normal' repositories
+sudo apt-get install -y adobe-flashplugin catfish clementine curl dropbox evolution focuswriter frei0r-plugins geary gpick
+sudo apt-get install -y glances gparted grsync hardinfo inkscape kazam nemo-dropbox
+sudo apt-get install -y openshot ppa-purge radiotray screenruler screenfetch scrot shutter slurm 
+sudo apt-get install -y terminator thunar variety vlc vnstat winbind  
+
+#software out of selection
+
+#sudo apt-get install -y agave skype
+
+
+#software from extra repositories
+#sudo apt-get install -y boot-repair plank 
+sudo apt-get install -y plank 
+
+#software out of selection
+#sudo apt-get install -y  grub-customizer
+
+
+#operating specific software
+
+sudo apt-get install nemo-compare -y
+
+###############################################################################################
+
+# installation of zippers and unzippers
+sudo apt-get install -y p7zip-rar p7zip-full unace unrar zip unzip sharutils rar uudeview mpack arj cabextract file-roller
+
+###############################################################################################
+
+#themes
+#sudo apt-get install numix-gtk-theme numix-icon-theme-circle -y
+sudo apt-get install -y breeze-cursor-theme
+#sudo apt-get install -y xcursor-themes
+
+
+###############################################################################################
+
+#ending
+#mkdir $HOME/Upload
+#sudo apt-get -y update
+#sudo apt-get -f -y install
+#sudo apt-get -y upgrade
+#sudo apt-get -y autoremove
+#sudo apt-get -y autoclean
+
